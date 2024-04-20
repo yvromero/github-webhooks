8d21db15b34dec10e3731949af8b5a18be3c92f4
yvromero <yvonne.romero@uds.edu.py>
2024-04-19 20:18:23 -0400

    Authenticate requests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubCrypMiddleware } from './presentation/middlewares/github-cryp.middleware';
 
 
 (() => {
@@ -16,6 +17,8 @@ function main () {
     const controller = new GithubController();
 
     app.use( express.json() );
+
+    app.use( GithubCrypMiddleware.verifySignature );
 
     app.post('/api/github', controller.webhookHandler );
 
